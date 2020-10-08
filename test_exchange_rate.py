001af55fa3fecb54d553ab33eb7d164b84a556fd
cjdufault <dufaultconnor@gmail.com>
2020-10-08 14:02:09 -0500

    add test_nominal_case for GetTargetCurrencyTestCase, GetDollarAmountTestCase, and ConvertDollarsToCurrencyTestCase

--- test_exchange_rate.py
+++ test_exchange_rate.py
@@ -10,6 +10,7 @@ class GetTargetCurrencyTestCase(TestCase):
     def test_nominal_case(self, mock_input):
         currency = get_target_currency()
         expected_currency = mock_input.return_value.upper()
+        
         self.assertEqual(currency, expected_currency)
         
 
@@ -19,7 +20,19 @@ class GetDollarAmountTestCase(TestCase):
     def test_nominal_case(self, mock_input):
         dollar_amount = get_dollar_amount()
         expected_dollar_amount = float(mock_input.return_value)
+        
         self.assertEqual(dollar_amount, expected_dollar_amount)
+        
+
+class ConvertDollarsToCurrencyTestCase(TestCase):
+        
+    @patch('get_exchange_rate', return_value=1.3)
+    @patch('convert'), return_value=6.5)
+    def test_nominal_case(self, mock_get_exchange_rate, mock_convert):
+        converted = convert_dollars_to_target(5, 'CAD')
+        expected_converted = mock_convert.return_value
+        
+        self.assertEqual(converted, expected_converted)
 
 
 if __name__ == '__main__':
